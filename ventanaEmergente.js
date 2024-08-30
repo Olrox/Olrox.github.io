7482a02b06575e37061a7e9299a5b01275d369ac
Olrox <83raymundoisrael@gmail.com>
2024-08-29 22:26:40 -0600

    Actualización de Datos
    Se agregan cuatro desplazamientos , un estilo y un script

---
+++ ventanaEmergente.js
@@ -0,0 +1,7 @@
+function mostrarVentanaEmergente() {
+    document.getElementById('ventanaEmergente').style.display = 'block';
+}
+
+function cerrarVentanaEmergente() {
+    document.getElementById('ventanaEmergente').style.display = 'none';
+}
